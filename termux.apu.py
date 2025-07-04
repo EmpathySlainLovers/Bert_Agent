e49044663fcf6a9a6c37a1325b64475d57ce4301
EmpathySlainLovers <castertenslow@gmail.com>
2025-07-04 12:00:16 -0600

    Update termux.apu.py

--- termux.apu.py
+++ termux.apu.py
@@ -1,3 +1,11 @@
+import os
+
+def execute_termux_command(cmd):
+    try:
+        result = os.popen(cmd).read()
+        return f"[Termux] {result.strip()}"
+    except Exception as e:
+        return f"[Error] {str(e)}"
 import subprocess
 
 def tapi(cmd):
